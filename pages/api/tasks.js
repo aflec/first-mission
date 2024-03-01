32699b8546a3d6f9ad9b876ea554e7870d854527
André <andflec@gmail.com>
2024-03-01 20:22:31 -0300

    Troca dos IF ELSE por SWITCH

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -6,7 +6,8 @@ export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("tasks");
 
-  if (req.method === 'GET') {
+/*
+ if (req.method === 'GET') {
     const tasks = await db.collection("tasks").find({}).toArray();
     res.json({ status: 200, data: tasks });
   } else if (req.method === 'POST') {
@@ -29,5 +30,35 @@ export default async function handler(req, res) {
     res.json(updateResult);
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
+  }
+*/
+
+  let bodyObject = (req.body);
+  switch (req.method) {
+    case 'GET':
+    const tasks = await db.collection("tasks").find({}).toArray();
+    res.json({ status: 200, data: tasks });
+    break;
+    case 'POST':
+    let myTask = await db.collection("tasks").insertOne(bodyObject);
+    res.json(myTask.ops[0]);
+    break;
+    case 'DELETE':
+    let deletedReturn = await db.collection("tasks").deleteOne({
+      "_id": new ObjectId(bodyObject._id)
+    });
+    res.json({quantidadeDocumentosApagados: deletedReturn.deletedCount});
+    break;
+    case 'PUT':
+    const filter = {
+      "_id": new ObjectId(bodyObject._id)
+    };
+    const update = { $set: {checked: bodyObject.checked} };
+    let updateResult = await db.collection("tasks").updateOne(filter, update);
+    res.json(updateResult);
+    break;
+    default:
+    res.status(405).json({ error: 'Method Not Allowed' });
+    break;
   }
 }
